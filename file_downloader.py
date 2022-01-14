06fdd1526ae4d70b6a89e20a8d54137baf4edb27
singhfalak <75836454+singhfalak@users.noreply.github.com>
2022-01-14 14:52:20 +0530

    Add files via upload

---
+++ file_downloader.py
@@ -0,0 +1,9 @@
+import requests
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as output_file:
+        output_file.write(get_response.content)
+
+download("https://wallpaperaccess.com/full/1586538.jpg")
